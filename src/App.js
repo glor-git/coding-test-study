49a020eecc763cf38045274337dfef67336f9bcf
Glor Shin <sykkkkk412@naver.com>
2022-11-29 16:10:31 +0900

    피자 나눠 먹기(1)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Test1 from "./programmers/짝수는 싫어요"; // 문제 1
+import Test1 from "./programmers/짝수는 싫어요";
+import Test2 from "./programmers/피자 나눠 먹기(1)"; // 문제 1
 
 function App() {
   return (
     <div className="App">
       <Test1 number={40} />
+      <Test2 number={15} />
     </div>
   );
 }
