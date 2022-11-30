95b2ccbcb6238fc039f239e18c14c318f7224dde
Glor Shin <sykkkkk412@naver.com>
2022-11-30 18:38:55 +0900

    제일 작은 수 제거하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Test1 from "./programmers/짝수는 싫어요";
-import Test2 from "./programmers/피자 나눠 먹기(1)"; // 문제 1
+import Test2 from "./programmers/피자 나눠 먹기(1)";
+import Test3 from "./programmers/제일 작은 수 제거하기";
 
 function App() {
   return (
     <div className="App">
       <Test1 number={40} />
       <Test2 number={15} />
+      <Test3 arr={[4,3,2]} />
     </div>
   );
 }
